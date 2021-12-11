ac35b10725033a9a245a8800e946922d44d325d1
ahmarcode <62645337+ahmarcode@users.noreply.github.com>
2021-12-11 15:13:42 +0530

    Update rps.cpp

--- rps.cpp
+++ rps.cpp
@@ -5,30 +5,71 @@ using namespace std;
 
 int main()
 {
-    int a, invest, profit, loss, max, random, current, initial;
+    //VARIABLE DECLERATION AND INICIALIZATION
+    int a, invest, profit, loss, max, random, current, powerup, rob, validity;
     string output, input;
     string rules;
     bool win;
+    validity = 20;
     current = 10000;
     while (current > 0)
     {
-        
-        cout << "press 1 : play.\nPress 2 : For rules.\nYour response : ";
+    //INSTRUCTIONS
+        cout << "Press 1 : Play: \nPress 2 : For rules and commands: \nPress 3 : For Power-ups: \nPress 4 : Rob: (Coming Soon)\n";
         cin >> a;
+
+        //RULES AND COMMANDS
         if(a == 2)
         {
              cout << "If you win the money will be rewarded as doubled of investment.\nIf you lose the investment is gone.\nIf it is draw then get your investment back.\nFor every invalid input 10$ will be reduced.\n";
         }
 
+        //POWER-UP STORE
+        else if(a == 3)
+        {
+            cout << "Welcome to STORE OF POWER-UPS.\nPress 1 : Bonus Factor : $2000\nPress 2 : Loss-Guard : $1000\nPress 3 : Rob-Guard : $1500 (Coming Soon)\nPress 4 : Details of POWER-UPS :\n";
+            cout << "Your current balance is $" << current << ".\n";
+            cin >> powerup;
+            
+
+            if(powerup == 4)
+            {
+                cout << "Bonus Factor : Increases the profit to thrice of investment\n";
+                cout << "Loss-Guard : Defend you from losses.\n";
+                cout << "Rob-Guard : Security from robbing (Coming Soon)\n";
+            }
+            
+            else if(powerup == 1 || powerup == 2)
+            {
+            //    if(validity > 0)
+            //  {
+            //        cout << "Your validity is not over yet. Validity is " << validity << endl;
+            //  }
+            //    else 
+            //    {
+                current = current - 2000;
+              //  }
+            }
+            if(powerup != 4)
+            {
+                cout << "Thanks for Purchasing!!\nThese power-ups are only valid for 20 turns of investments.\n";
+            }
+        }
+
         else if(a == 1)
-        {   initial = current;
-            cout << "Your initial money is $" << initial << endl;
+        {   
+            //CURRENT MONEY
+            cout << "Your initial money is $" << current << endl;
+            //OPPONENT RESPONSE GENERATOR
             max = 3;
             srand(time(0));
             random = (rand() % max) + 1;
+
+            //INVESTMENT INPUT
             cout << "Enter the amount you want to invest: ";
             cin >> invest;
-
+            
+            //LOW INVESTMENT CONDITION
             while (invest > current)
             {
                 cout << "Be in your limit.\nYou can only invest money you have.\n";
@@ -36,15 +77,18 @@ int main()
                 cout << "Enter the amount you want to invest: ";
                 cin >> invest;
             }
-       
+            //ATTACK INPUT
             cout << "Enter your choice: ";
             cin >> input;
+
+            //INVALID INPUT INITIATIVE
             if (input != "rock" && input != "paper" && input != "scissor")
             {
                 cout << "Invalid Input\n";
                 current = current - 10;
             }
 
+            //OPPONENT RESPONSE MANUPILATOR
             if (random == 1)
             {
                 output = "rock";
@@ -60,6 +104,7 @@ int main()
                 output = "scissor";
             }
 
+            //VICTORY JUDGEMENT
             if (output == input)
             {
                 cout << "Draw.\n";
@@ -110,31 +155,50 @@ int main()
                 win = true;
                 cout << "The rival played scissor." << endl;
             }
-
+            //REWARDS
             if (win == true)
             {
                 if (output != input)
                 {
-                    current = initial - invest;
-                    profit = 2 * invest;
+                    
+                    current = current - invest;
+                    //BONUS-FACTOR
+                    if(powerup == 1 && validity <= 20)
+                    {
+                        profit = (3 * invest);
+                        validity--;
+                    }
+                    else 
+                    {
+                        profit = 2 * invest;
+                    }
                     current = current + profit;
                     cout << "Your current money is $" << current << endl;
                 }
 
                 else if (output == input)
                 {
-                    current = initial;
                     cout << "Your current money is $" << current << endl;
                 }
             }
 
             else if (win == false)
             {
-                loss = invest;
-                current = initial - loss;
+                current = current - invest;
+                //LOSS-GUARD
+                if(powerup == 2 && validity <= 20)
+                {
+                    loss = 0;
+                }
+                else 
+                {
+                    current = current - loss;
+                }
                 cout << "Your current money is $" << current << endl;
             }
         }
+        
+
     }   
     return 0;
-}   
+}
